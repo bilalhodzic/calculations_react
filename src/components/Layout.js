d8cb976d744e9e2157d17ad09c1f21d6fb87ca6f
bilalhodzic <hodzic.bilal@hotmail.com>
2021-03-01 13:44:28 +0100

    added layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,32 @@
 import React from "react";
 import {
-  Container,
   Button,
   Typography,
   Drawer,
-  Divider,
   Box,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { ReactComponent as CalcIcon } from "../images/calc_icon.svg";
+import { ReactComponent as HomeIcon } from "../images/HomeIcon.svg";
+
+const drawerList = [
+  {
+    name: "Dashboard",
+    icon: <HomeIcon />,
+  },
+  {
+    name: "Calculations",
+    icon: <CalcIcon />,
+  },
+  {
+    name: "New Calculation",
+    icon: "+ ",
+  },
+];
 
 export default function Layout(props) {
   React.useEffect(() => {
@@ -24,8 +43,28 @@ export default function Layout(props) {
         variant="permanent"
         anchor="left"
       >
-        This is Layout
-        <Divider />
+        <Box className={classes.header}>
+          <Box className={classes.drawerHeader}>
+            <Typography>Calculation</Typography>
+          </Box>
+          <Button className={classes.headerButton}>+ New Calculations</Button>
+        </Box>
+        <Box className={classes.footer}></Box>
+        <List className={classes.list}>
+          {drawerList.map((list, index) => (
+            <ListItem className={classes.listButton} button key={list.name}>
+              <ListItemIcon className={classes.listIcon}>
+                {list.icon}
+              </ListItemIcon>
+              <ListItemText
+                disableTypography={true}
+                className={classes.listText}
+              >
+                {list.name}
+              </ListItemText>
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
       {props.children}
     </Box>
@@ -52,5 +91,80 @@ const useStyles = makeStyles((theme) => ({
     color: "white",
     width: calculate(266),
     backgroundColor: "#21344D",
+    textAlign: "center",
+    minWidth: 180,
+  },
+  drawerHeader: {
+    height: "-webkit-fill-available",
+    width: calculate(267),
+    backgroundColor: "#21344d",
+    display: "flex",
+    minWidth: 181,
+
+    justifyContent: "center",
+    alignItems: "inherit",
+    "& p": {
+      fontSize: 22,
+    },
+  },
+  footer: {
+    width: "100%",
+    height: calculate(67, 1024),
+    backgroundColor: "white",
+    position: "fixed",
+    bottom: 0,
+  },
+  header: {
+    backgroundColor: "white",
+    width: "100%",
+    height: calculate(79, 1024),
+    position: "fixed",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottom: "1px solid #B0B0B0",
+  },
+  headerButton: {
+    backgroundColor: "#21344D",
+    width: calculate(207),
+    height: 40,
+    borderRadius: 42,
+    textTransform: "none",
+    fontSize: 20,
+    margin: theme.spacing(2),
+    color: "white",
+    minWidth: 220,
+    "&:hover": {
+      background: "#1b2c44",
+    },
+  },
+  list: {
+    paddingTop: theme.spacing(8),
+    alignSelf: "center",
+  },
+  listButton: {
+    marginBlockStart: theme.spacing(8),
+    height: 55,
+    borderRadius: 42.69,
+    "&:hover": {
+      background: "#36547B",
+    },
+    "&:active": {
+      background: "#36547B",
+    },
+    "&:focus": {
+      background: "#36547B",
+    },
+  },
+  listText: {
+    fontSize: 19,
+    fontWeight: 500,
+    fontFamily: "Poppins",
+  },
+  listIcon: {
+    color: "white",
+    fontSize: 25,
+    minWidth: 33,
+    marginBottom: theme.spacing(1),
   },
 }));
